655ae7fc1ce07d85c284d0ebc1a466f753624470
Chris <oliwierpawpatrol@gmail.com>
2026-07-11 20:22:07 +0200

    Initial Commit

---
+++ src/main/java/net/chris/chrismod/ChrisSRandomBullshitDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chris.chrismod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChrisSRandomBullshitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
